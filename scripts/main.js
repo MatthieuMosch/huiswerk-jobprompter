7f1a814348fc4c1f0b4dc7948915d1e0d9f89437
MatthieuMosch <mmosch@gmail.com>
2025-01-08 14:17:33 +0100

    finished opdracht 1

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,19 @@
-// the main script file
+//opdracht 1a
+const countSales = departments.sales.numberOfEmployees;
+console.log("De afdeling Sales heeft " + countSales + " medewerkers.");
 
+//opdracht 1b
+console.log("Marketing is een leuke afdeling om te werken.");
+console.log(departments.marketing.description);
+
+//opdracht 1c
+const countCustomerService = departments["customer-service"].numberOfEmployees;
+console.log("De afdeling Customer Service heeft " + countCustomerService + " medewerkers.");
+
+//opdracht 1d
+console.log("Sales is een uitdagende afdeling om te werken als Verkoopmanager.");
+for (let i = 0; i < departments.sales.jobs.length; i++) {
+    if (departments.sales.jobs[i].title === "Verkoopmanager") {
+        console.log(departments.sales.jobs[i].description);
+    }
+}
